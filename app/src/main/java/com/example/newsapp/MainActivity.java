fffd7581c4a2119235cea54538a068be077a8044
TheresaRoukaibe <theresaroukaibe@gmail.com>
2022-09-28 16:57:13 +0300

    splash screen implemented

---
+++ app/src/main/java/com/example/newsapp/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.newsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity {
+Timer timer;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, logInActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
+    }
+}
