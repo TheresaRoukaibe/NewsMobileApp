6598bb31fdb5c3f76e464e05ac58495254516fc7
TheresaRoukaibe <theresaroukaibe@gmail.com>
2022-09-28 20:59:45 +0300

    Added user logged in using shared preferences

---
+++ app/src/main/java/com/example/newsapp/BrowseActivity.java
@@ -0,0 +1,28 @@
+package com.example.newsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.SharedPreferences;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class BrowseActivity extends AppCompatActivity {
+TextView textView;
+SharedPreferences sharedPreferences;
+private static final String key_name = "name";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_browse);
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.teal_700)));
+        textView = findViewById(R.id.welcome);
+        sharedPreferences = getSharedPreferences("com.lau.news", MODE_PRIVATE);
+        String name = sharedPreferences.getString(key_name, null);
+        if(name!= null){
+            textView.setText("Hi ! " + name);
+        }
+
+    }
+}
--- app/src/main/java/com/example/newsapp/logInActivity.java
+++ app/src/main/java/com/example/newsapp/logInActivity.java
@@ -2,13 +2,43 @@ package com.example.newsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class logInActivity extends AppCompatActivity {
+EditText editText;
+SharedPreferences sharedPreferences;
+private static final String key_name = "name";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.teal_700)));
+        editText = findViewById(R.id.username);
+        sharedPreferences = getSharedPreferences("com.lau.news", Context.MODE_PRIVATE);
+    }
+
+    public void sendInfo(View v) {
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        String username_text = editText.getText().toString();
+        if (username_text.equalsIgnoreCase("")) {
+            Toast.makeText(logInActivity.this, "Please enter name", Toast.LENGTH_SHORT).show();
+        } else {
+
+
+            editor.putString(key_name, editText.getText().toString());
+            editor.apply();
+            Intent intent = new Intent(logInActivity.this, BrowseActivity.class);
+            startActivity(intent);
+            Toast.makeText(logInActivity.this, "Logging in", Toast.LENGTH_SHORT).show();
+        }
     }
 }
